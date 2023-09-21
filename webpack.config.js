8351b661b5e7645100807aeb3287a2630f1ddf84
Joe Chek <jmchek@gmail.com>
2023-09-21 15:45:38 -0700

    remove HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: './src/index.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Ghalta\'s Primal Hunger',
-    }),
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
