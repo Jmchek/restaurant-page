3920a6da2f4bb4e78483c0a380d5c5c555a83efa
Joe Chek <jmchek@gmail.com>
2023-09-26 15:42:56 -0700

    change file type of image, fix import/export issues with js files, begin page creation through js files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import _ from 'lodash';
 import './ghalta.css';
 import loadMe from './page-load.js';
-// import contentGrbbr from './page-load.js';
-// import './page-load.js';
-
-//  function component() {
-//    const element = document.createElement('div');
-//   const btn = document.createElement('button');
-
-//    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   btn.innerHTML = 'Click me and check the console!';
-//   btn.onclick = loadMe;
-
-//   element.appendChild(btn);
-
-//    return element;
-//  }
-
-//  document.body.appendChild(component());
+import Ghalta from './Ghalta.jpg';
 
 loadMe();
-
- const contentGrbbr = document.querySelector('.content');
-
-// contentGrbbr.appendChild(loadMe.resHeaderCrtr);
-
--- src/page-load.js
+++ src/page-load.js
@@ -1,14 +1,15 @@
 export default function loadMe() {
-    const resHeaderCrtr = document.createElement('h2', '#resHeader');
+    const contentGrbbr = document.querySelector('.content');
+    const resHeaderCrtr = document.createElement('h2');
+    const heroImgCrtr = document.createElement('img');
 
-    console.log("hello");
+    resHeaderCrtr.id = "resHeader";
+    resHeaderCrtr.innerText = "Ghalta's Primal Hunger";
+
+    heroImgCrtr.id = "heroImg";
+    heroImgCrtr.src = "/home/joe/repos/restaurant-page/src/Ghalta.jpg";
+
+    contentGrbbr.appendChild(resHeaderCrtr);
+    contentGrbbr.appendChild(heroImgCrtr);
   }
 
-// export default loadMe = (() => {
-//   const contentGrbbr = document.querySelector(".content");
-//   const resHeaderCrtr = document.createElement('h2');
-
-//   return {
-//     resHeaderCrtr,
-//   };
-// })();
