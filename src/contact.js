4c5f6e48d290b800c031c434f1de7f348dfba584
Joe Chek <jmchek@gmail.com>
2023-10-09 16:02:09 -0700

    correct pathing for image files

--- src/contact.js
+++ src/contact.js
@@ -24,15 +24,15 @@ export default function contactTab() {
     //item 1
     menuArrCrtr[0].children[0].children[0].innerText = "Huatli, the Sun's Heart";
     menuArrCrtr[0].children[0].children[1].innerText = "Chef \n 555-555-5555 \n dinosrule@ixalan.com";
-    menuArrCrtr[0].children[1].src = "/home/joe/repos/restaurant-page/src/Huatli-Heart-of-the-Sun-War-of-the-Spark-Art.jpg";
+    menuArrCrtr[0].children[1].src = "src/Huatli-Heart-of-the-Sun-War-of-the-Spark-Art.jpg";
 
     //item 2
     menuArrCrtr[1].children[0].children[0].innerText = "Angrath, the Flame-Chained";
     menuArrCrtr[1].children[0].children[1].innerText = "Manager \n 555-555-5555 \n discardintogy@ixalan.com";
-    menuArrCrtr[1].children[1].src = "/home/joe/repos/restaurant-page/src/Angrath-Minotaur-Pirate-Rivals-of-Ixalan-MtG-Art.jpg";
+    menuArrCrtr[1].children[1].src = "src/Angrath-Minotaur-Pirate-Rivals-of-Ixalan-MtG-Art.jpg";
 
     //item 3
     menuArrCrtr[2].children[0].children[0].innerText = "Vraska, Golgari Queen";
     menuArrCrtr[2].children[0].children[1].innerText = "Hostess \n 555-555-5555 \n niceperm@ixalan.com";
-    menuArrCrtr[2].children[1].src = "/home/joe/repos/restaurant-page/src/Vraska-Scheming-Gorgon-Rivals-of-Ixalan-MtG-Art.png";
+    menuArrCrtr[2].children[1].src = "src/Vraska-Scheming-Gorgon-Rivals-of-Ixalan-MtG-Art.png";
 }
--- src/page-load.js
+++ src/page-load.js
@@ -9,7 +9,7 @@ export default function loadMe() {
     resHeaderCrtr.innerText = "Ghalta's Primal Hunger";
 
     heroImgCrtr.id = "heroImg";
-    heroImgCrtr.src = "/home/joe/repos/restaurant-page/src/Ghalta.jpg";
+    heroImgCrtr.src = "src/Ghalta.jpg";
 
     resDescCrtr.id = "resDesc";
     paraResDescCrtr.textContent = "Here at Ghalta's we understand hunger, we always have. You want a fresh patty, crisp onions, juicy tomatoes...cut about |\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0| that thick right? But most importantly, we know, the ONIONS DON'T TOUCH THE TOMATOES.";
